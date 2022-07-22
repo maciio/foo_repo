8c76e6c8e644d1f7d397712a72089f1a54fe3c86
mario pineda <wolf.eos.v6@gmail.com>
2022-07-22 13:49:58 -0500

    add random stuff

--- dev/hello.py
+++ dev/hello.py
@@ -6,9 +6,12 @@ def reply():
     reply=input("And whats your name?")
     print(f"it's nice to meet you too {reply} as well")
     return reply
+
+def do_stuff():
+    print("...what ever")
+
 def say_bye(name):
     print(f"Good bye {name}!")    
-  
 
 say_hello(input("your name is... "))
 say_bye(reply())
