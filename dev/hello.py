0c4f306e4e550ec8fa5b28aa35b052c42d9f4992
JuanPablo <jp.mmoctezuma@hotmail.com>
2022-07-22 12:57:55 -0500

    Adding reply to the hello code

--- dev/hello.py
+++ dev/hello.py
@@ -2,5 +2,9 @@
 def say_hello(name):
     print(f"hello {name}")
 
+def reply():
+    reply=iput("And whats your name?")
+    print(f"it's nice to meet you too {reply} as well")
 
 say_hello(input("your name is... "))
+reply()
